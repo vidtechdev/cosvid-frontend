579fb2987180621747e1f18ea976e02f2025d7d2
Bob <>
2021-09-17 12:12:49 -0500

    fixed import error

--- components/ClusterVideoBox.js
+++ components/ClusterVideoBox.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { server } from '../../config';
+import { server } from '../config';
 
 const ClusterVideoStyles = styled.div`
   display: grid;
