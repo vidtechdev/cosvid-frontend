b7cb7d7d644cace204ddf3f4860ee12d9e2a7982
Bob <>
2022-11-16 11:28:27 -0600

    removed spanish and work option link references in nav. Need to fix nav styles for fewer links

--- components/Nav.js
+++ components/Nav.js
@@ -6,9 +6,11 @@ export default function Nav() {
       <Link href="/">Video Library</Link>
       {/* <Link href="/career-videos">Career Videos (REST)</Link> */}
       <Link href="/career/videos">Career Videos</Link>
+      <Link href="/">Career Videos in Spanish</Link>
       {/* <Link href="/spanish-videos">Career Videos in Spanish</Link> */}
       <Link href="/career-clusters">Career Cluster / Industry Videos</Link>
       <Link href="/skill-and-ability/videos">Skill and Ability Videos</Link>
+      <Link href="/">Work Option Videos</Link>
       {/* <Link href="/work-option/videos">Work Option Videos</Link> */}
       <Link href="/tutorial/videos">Tutorial Videos</Link>
       <Link href="/faq">FAQs About Videos</Link>
